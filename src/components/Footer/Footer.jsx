0f188422d58ef10baea0d4c6d1a8118fba7cf96c
Brixi <denikucevic@gmail.com>
2021-03-29 04:12:20 +0200

    fix hover on mobiles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,32 +14,31 @@ const Footer = () => {
 
   return (
     <div className="page-4">
-      <span style={{ fontSize: "40px" }} className="span-text">That's it! <br /> (for now 😊)
+      <span style={{ fontSize: "40px" }} className="span-text">That's it!
           </span>
-      <br />
-      <br />
-      <span style={{ fontSize: "30px" }} className="span-text">
-        If you wish to contact me you can do that here:
+      <span style={{ fontSize: "20px" }}> (for now 😊)</span>
+      <span style={{ fontSize: "30px" }} className="span-text text-spacing">
+        If you wish to <span style={{ color: "red" }}><u>contact me</u></span> you can do that here:
           </span>
       <br /><br />
-      <span style={{ fontSize: "30px" }} className="span-text">
-        <a href="mailto:denikucevic@gmail.com">denikucevic@gmail.com</a>
+      <span style={{ fontSize: "30px" }} className="span-text text-spacing">
+        <a href="mailto:denikucevic@gmail.com" style={{ textDecoration: "none" }}>denikucevic@gmail.com</a>
       </span><br />
       <span style={{ fontSize: "30px" }} className="span-text">
         +381 66 6361964
           </span><br /><br /><br />
-      <span style={{ fontSize: "30px" }} className="span-text">
+      <span style={{ fontSize: "30px" }} className="span-text text-spacing">
         OR
           </span><br /><br /><br />
       <span>
         <a href="https://github.com/DeniKucevic" target="_blank" rel="noreferrer noopener">
-          <img src={GitHub} alt="GitHub" className="github-icon" />
+          <img src={GitHub} alt="GitHub" className="github-icon rotate" />
         </a>
         <a href="https://www.linkedin.com/in/denis-kucevic/" target="_blank" rel="noreferrer noopener">
-          <img src={Linkedin} alt="Linkedin" className="github-icon" />
+          <img src={Linkedin} alt="Linkedin" className="github-icon rotate" />
         </a>
         <a href="https://www.facebook.com/denis.kucevic.9/" target="_blank" rel="noreferrer noopener">
-          <img src={Facebook} alt="Facebook" className="github-icon" />
+          <img src={Facebook} alt="Facebook" className="github-icon rotate" />
         </a>
       </span>
     </div>
